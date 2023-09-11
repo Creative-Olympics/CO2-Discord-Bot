c4bfb59d968f0aa78515d7916ed23568936c5227
ZRunner <z.runner.mc@gmail.com>
2023-09-11 21:44:43 +0200

    refact(gaw): adapt code

--- src/modules/giveaways/main.py
+++ src/modules/giveaways/main.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 import discord
 from discord.ext import commands
 
-from src.cobot import CObot
-from src.modules.giveaways.types import GiveawayData
+from src.cobot import CObot, COInteraction
+from src.modules.giveaways.types import GiveawayToSendData
 
 
 class GiveawaysCog(commands.Cog):
@@ -20,33 +22,61 @@ class GiveawaysCog(commands.Cog):
     )
 
     @group.command(name="list")
-    async def gw_list(self, interaction: discord.Interaction, *, include_stopped: bool=False):
+    async def gw_list(self, interaction: COInteraction, *, include_stopped: bool=False):
         "List all the giveaways in the server"
         await interaction.response.defer()
         text = ""
         now = discord.utils.utcnow()
         docs = self.bot.fb.get_giveaways() if include_stopped else self.bot.fb.get_active_giveaways()
-        async for doc in docs:
-            gaw: GiveawayData = doc.to_dict() # type: ignore
-            if not include_stopped and gaw["ends_at"] < now:
-                continue
+        async for gaw in docs:
             name = gaw["name"]
-            participants_count = len(gaw["participants"])
             text += f"**{name}**  -  "
-            if participants_count == 0:
-                text += "no participant"
-            elif participants_count == 1:
-                text += "1 participant"
-            else:
-                text += f"{participants_count} participants"
+            # participants_count = len(gaw["participants"])
+            # if participants_count == 0:
+            #     text += "no participant - "
+            # elif participants_count == 1:
+            #     text += "1 participant - "
+            # else:
+            #     text += f"{participants_count} participants - "
             end_date = discord.utils.format_dt(gaw["ends_at"], "R")
-            text += f" - {end_date}\n"
+            if gaw["ends_at"] > now:
+                text += f"ends in {end_date}\n"
+            else:
+                text += f"ended {end_date}\n"
         embed = discord.Embed(
             title=("List of all giveaways" if include_stopped else "List of active giveaways"),
             description=text,
             color=self.embed_color
         )
         await interaction.followup.send(embed=embed)
+
+    @group.command(name="create")
+    async def gw_create(self, interaction: COInteraction, *, name: str, description: str,
+                        duration: str, channel: Optional[discord.TextChannel]=None,
+                        color: Optional[str]=None, max_entries: int=1,
+                        winners_count: int=1):
+        "Create a giveaway"
+        if interaction.guild is None:
+            return
+        target_channel = channel or interaction.channel
+        if target_channel is None:
+            return
+        parsed_color = discord.Color.from_str(color) if color else None
+        await interaction.response.defer()
+        data: GiveawayToSendData = {
+            "guild": interaction.guild.id,
+            "channel": target_channel.id,
+            "name": name,
+            "description": description,
+            "color": parsed_color.value if parsed_color else self.embed_color,
+            "max_entries": max_entries,
+            "winners_count": winners_count,
+            "ends_at": discord.utils.parse_time(duration),
+            "ended": False,
+            "participants": [],
+            "winners": []
+        }
+
 
 
 
