bfbaa3a5f0dfb8f18eafb7e375a8094ee32e9c9a
ZRunner <z.runner.mc@gmail.com>
2023-09-13 19:41:46 +0200

    feat(fb): add cache controler

---
+++ src/firebase/caching.py
@@ -0,0 +1,68 @@
+import discord
+from src.modules.giveaways.types import GiveawayData
+
+
+class FirebaseCacheControler:
+    """Controls the cache for the Firebase requests"""
+
+    def __init__(self):
+        self.participants_cache: dict[str, list[int]] = {}
+        self.__synced_participants_giveaways: set[str] = set()
+        self.giveaways_cache: dict[str, GiveawayData] = {}
+        self.__are_giveaways_sync = False
+        self.__are_active_giveaways_sync = False
+
+    def are_participants_sync(self, giveaway_id: str):
+        "Are the participants cached?"
+        return giveaway_id in self.__synced_participants_giveaways
+
+    @property
+    def are_giveaways_sync(self):
+        "Are the giveaways cached?"
+        return self.__are_giveaways_sync
+
+    @property
+    def are_active_giveaways_sync(self):
+        "Are the active giveaways cached?"
+        return self.__are_active_giveaways_sync or self.__are_giveaways_sync
+
+    def get_participants(self, giveaway_id: str):
+        "Get the participants for a giveaway"
+        return self.participants_cache.get(giveaway_id)
+
+    def get_giveaway(self, giveaway_id: str):
+        "Get a giveaway"
+        return self.giveaways_cache.get(giveaway_id)
+
+    def get_giveaways(self):
+        "Get all giveaways"
+        return self.giveaways_cache.values()
+
+    def get_active_giveaways(self):
+        "Get all active giveaways"
+        return filter(lambda g: not g["ended"], self.giveaways_cache.values())
+
+    def get_pending_giveaways(self):
+        "Get all active giveaways whose end date is in the past"
+        now = discord.utils.utcnow()
+        return filter(lambda g: not g["ended"] and g["ends_at"] < now, self.giveaways_cache.values())
+
+    def set_participants(self, giveaway_id: str, participants: list[int]):
+        "Set the participants for all giveaways"
+        self.participants_cache[giveaway_id] = participants
+        self.__synced_participants_giveaways.add(giveaway_id)
+
+    def set_giveaways(self, giveaways: list[GiveawayData]):
+        "Set the giveaways"
+        self.giveaways_cache = {g["id"]: g for g in giveaways}
+        self.__are_giveaways_sync = True
+
+    def set_active_giveaways(self, giveaways: list[GiveawayData]):
+        "Set the active giveaways"
+        self.giveaways_cache = {g["id"]: g for g in giveaways}
+        self.__are_active_giveaways_sync = True
+
+    def set_new_giveaway(self, giveaway: GiveawayData):
+        "Set a new giveaway"
+        self.giveaways_cache[giveaway["id"]] = giveaway
+        self.participants_cache[giveaway["id"]] = []
--- src/firebase/client.py
+++ src/firebase/client.py
@@ -4,6 +4,7 @@ from typing import AsyncGenerator, Optional
 import firebase_admin
 from firebase_admin import credentials, db
 
+from src.firebase.caching import FirebaseCacheControler
 from src.modules.giveaways.types import GiveawayData, RawGiveawayData
 
 
@@ -18,31 +19,52 @@ class FirebaseDB:
             #     'uid': auth_uuid
             # }
         })
+        self.cache = FirebaseCacheControler()
 
     async def get_giveaways(self) -> AsyncGenerator[GiveawayData, None]:
         "Get a generator of giveaway documents"
+        if self.cache.are_giveaways_sync:
+            for gaw in self.cache.get_giveaways():
+                yield gaw
+            return
+        print("Fetching giveaways")
         ref = db.reference("giveaways")
         snapshot: dict[str, RawGiveawayData] = ref.get() # type: ignore
-        for gaw_id, gaw in snapshot.items():
-            yield {
-                **gaw,
+        parsed_giveaways: list[GiveawayData] = [
+            {
+                **gaw, # type: ignore
                 "id": gaw_id,
                 "ends_at": dt.fromisoformat(gaw["ends_at"]),
                 "winners": gaw.get("winners", [])
-            } # type: ignore
+            }
+            for gaw_id, gaw in snapshot.items()
+        ]
+        self.cache.set_giveaways(parsed_giveaways)
+        for data in parsed_giveaways:
+            yield data
 
     async def get_active_giveaways(self) -> AsyncGenerator[GiveawayData, None]:
         """Get a generator of active giveaway documents (ie. not 'ended')
         Note: this may include giveaways that have a past end date but have not been marked as ended yet"""
+        if self.cache.are_active_giveaways_sync:
+            for gaw in self.cache.get_active_giveaways():
+                yield gaw
+            return
+        print("Fetching active giveaways")
         ref = db.reference("giveaways")
         snapshot: dict[str, RawGiveawayData] = ref.order_by_child("ended").equal_to(False).get() # type: ignore
-        for gaw_id, gaw in snapshot.items():
-            yield {
-                **gaw,
+        parsed_giveaways: list[GiveawayData] = [
+            {
+                **gaw, # type: ignore
                 "id": gaw_id,
                 "ends_at": dt.fromisoformat(gaw["ends_at"]),
                 "winners": gaw.get("winners", [])
-            } # type: ignore
+            }
+            for gaw_id, gaw in snapshot.items()
+        ]
+        self.cache.set_active_giveaways(parsed_giveaways)
+        for data in parsed_giveaways:
+            yield data
 
     async def create_giveaway(self, data: GiveawayData):
         "Create a giveaway document"
@@ -51,8 +73,14 @@ class FirebaseDB:
             **data,
             "ends_at": data["ends_at"].isoformat()
         })
+        self.cache.set_new_giveaway(data)
 
     async def get_giveaways_participants(self, giveaway_id: str) -> Optional[list[int]]:
         "Get a list of participants for a giveaway"
+        if self.cache.are_participants_sync(giveaway_id):
+            return self.cache.get_participants(giveaway_id)
+        print(f"Fetching participants for giveaway {giveaway_id}")
         ref = db.reference(f"participants/{giveaway_id}")
-        return ref.get() # type: ignore
+        snapshot: Optional[list[int]] = ref.get() # type: ignore
+        self.cache.set_participants(giveaway_id, snapshot or [])
+        return snapshot
