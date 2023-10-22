aefd42e7be5de6d374b849337d0f2997fc9d6983
ZRunner <z.runner.mc@gmail.com>
2023-10-21 20:21:28 -0400

    feat(gaw): add participants list

--- src/modules/giveaways/views.py
+++ src/modules/giveaways/views.py
@@ -1,7 +1,11 @@
-from discord import ButtonStyle, ui
+from math import ceil
+from typing import Union
+
+from discord import ButtonStyle, Embed, Member, User, ui
 
 from src.cobot import CObot
-from src.modules.giveaways.types import GiveawayToSendData
+from src.modules.giveaways.types import GiveawayData, GiveawayToSendData
+from src.utils.paginator_view import Paginator
 
 
 class GiveawayView(ui.View):
@@ -18,3 +22,36 @@ class GiveawayView(ui.View):
             custom_id=f"gaw-{gaw_id}"
         )
         self.add_item(enter_btn)
+
+
+class ParticipantsPaginator(Paginator):
+    "Allows users to see the participants of a giveaway"
+    def __init__(self, client: CObot, user: Union[User, Member], gaw: GiveawayData, participants: list[int]):
+        super().__init__(client, user)
+        self.title = f"Participants of {gaw['name']}"
+        self.participants = participants
+        self.page_count = ceil(len(participants) / 20)
+
+    async def get_page_count(self) -> int:
+        "Get total number of available pages"
+        return self.page_count
+
+    async def get_page_content(self, interaction, page):
+        "Build the page content given the page number and source interaction"
+        lower_index = (page - 1) * 20
+        upper_index = min(page * 20, len(self.participants))
+        participants_count = len(self.participants)
+        page_participants = [
+            f"<@{user_id}> ({user_id})"
+            for user_id in self.participants[lower_index:upper_index]
+        ]
+        if participants_count <= 20:
+            desc_header = f"### {participants_count} participants\n\n"
+        else:
+            desc_header = f"### Participants {lower_index+1}-{upper_index} out of {participants_count}\n\n"
+        embed = Embed(
+            title=self.title,
+            description=desc_header + "\n".join(page_participants),
+        )
+        embed.set_footer(text=f"Page {page}/{self.page_count}")
+        return {"embed": embed}
